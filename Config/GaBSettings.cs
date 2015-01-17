ab0fd459fee4fbdba88772d681e10fd002ec66dd
Mickaël <mfourgeaud@gmail.com>
2015-01-17 01:44:53 +0100

    @Justin char names and other sensitive info should only be stored in a SafeString! Fixed it for enchantable.

--- Config/GaBSettings.cs
+++ Config/GaBSettings.cs
@@ -39,7 +39,7 @@ namespace GarrisonButler.Config
         public bool RetrieveMail { get; set; }
         public bool SendMail { get; set; }
         public bool SendDisenchantableGreens { get; set; }
-        public string GreensToChar { get; set; }
+        public SafeString GreensToChar { get; set; }
         public bool ForceJunkSell { get; set; }
         public bool GarrisonCache { get; set; }
         public bool HarvestGarden { get; set; }
