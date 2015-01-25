fa07599a800236d9c3ed3c1bf54648e6b6c6db28
Mickaël <mfourgeaud@gmail.com>
2015-01-25 21:53:52 +0100

    Pushed as beta

--- GarrisonButler.cs
+++ GarrisonButler.cs
@@ -25,7 +25,7 @@ namespace GarrisonButler
 {
     public class GarrisonButler : BotBase
     {
-        internal static readonly ModuleVersion Version = new ModuleVersion(1, 10, 12, 0);
+        internal static readonly ModuleVersion Version = new ModuleVersion(1, 10, 12, 1);
 
         internal static List<Follower> Followers;
         internal static List<Mission> Missions;
