eca48506979b9c29f45397849e650bdf2cbb1e92
Mickaël <mfourgeaud@gmail.com>
2015-03-09 17:58:15 +0100

    Added fix for items name not displaying in some tab. Was coming from removal of tooltips.

--- GarrisonButler.cs
+++ GarrisonButler.cs
@@ -24,7 +24,7 @@ namespace GarrisonButler
 {
     public class GarrisonButler : BotBase
     {
-        internal static readonly ModuleVersion Version = new ModuleVersion(2, 4, 9, 0);
+        internal static readonly ModuleVersion Version = new ModuleVersion(2, 4, 11, 0);
 
         internal static List<Follower> Followers;
         internal static List<Mission> Missions;
@@ -39,7 +39,7 @@ namespace GarrisonButler
 
         public static string NameStatic
         {
-            get { return "GarrisonButler ICE"; }
+            get { return "GarrisonButler Lite"; }
         }
 
         // internal AutoAnglerProfile Profile { get; private set; }
